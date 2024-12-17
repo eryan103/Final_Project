888ba49cd4e1ded8838bfa5d4cdd04e9ee0c7612
Emily Ames <emily.ames@maine.edu>
2024-12-17 15:46:06 -0500

    Update game.py
    added stats for user

--- game.py
+++ game.py
@@ -5,3 +5,15 @@ Final Project
 12/17/2024
 '''
 # main game logic
+
+def main(): #these are the player's beginning stats
+    health = 10
+    stamina = 10
+    skill = 1
+    inventory = []
+
+    print("Welcome to the Adventure Game!\n")
+
+    while True:  #shows user what their current stats are
+        print(f"Health: {health}/10, Stamina: {stamina}/10, Skill: {skill}/10")
+
