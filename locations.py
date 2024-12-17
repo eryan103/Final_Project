89ca8bf29edf1deb71263db1bf8c981b7e2f8b91
Emily Ames <emily.ames@maine.edu>
2024-12-17 16:43:24 -0500

    Update locations.py
    added more locations and tried to fix inventory or stats issue

--- locations.py
+++ locations.py
@@ -3,9 +3,13 @@
 from inventory import Inventory
 from stats import Stats
 
+inventory = Inventory()
+player_stats = Stats()
+
 # these will be read by main file to say what's happening at what location
-def blacksmith():
-    if direction == 'N':  #blacksmith
+
+
+def blacksmith():  #blacksmith
         print("You are at the Blacksmith.")
         action = input("Choices: obtain a weapon (+3 skill), go back to the field, or go to the apothecary\n")
         if action.lower() == "obtain a weapon":
@@ -13,5 +17,49 @@ def blacksmith():
             print("You obtained a weapon. Skill increased by 3.")
         elif action.lower() == "go back to the field":
             print("Going back to the field.")
+            #field()
         elif action.lower() == "go to the apothecary":
             apothecary()
+
+def apothecary():
+    print("You are at the Apothecary.")
+    action = input("Choices: Obtain healing herbs (added to inventory, +3 health), go back to the field, or go to the trainer\n:")
+    if action.lower() == "obtain healing herbs":
+        inventory.append("Healing Herbs (+3 health)")
+        print("You obtained healing herbs. You can use them later.")
+    elif action.lower() == "go back to the field":
+        print("Going back to the field.")
+        #field()
+    elif action.lower() == "go to the trainer":
+        trainer()
+
+def trainer():
+    print("You are at the Trainer.")
+    action = input("Chioces: Be trained (+5 skill), go back to the field, or go to the inn\n")
+    if action.lower() == "be trained":
+        player_stats['skill'] += 5
+        print("You received training. Skill increased by 5.")
+    elif action.lower() == "go back to the field":
+        print("going back to the field.")
+        #field()
+    elif action.lower() == "go to the inn":
+        inn()
+
+def inn():
+    print("You are at the Inn.")
+    action = input("Choices: Sleep (full health/stamina restore, +1 skill), drinking competition (-5 health, -5 stamina, +2 skill), go back to the field, or go to the mountain")
+    if action.lower() == "sleep":
+        player_stats['health'] = 10
+        player_stats['stamina'] = 10
+        player_stats['skill'] += 1
+        print("You rested at the Inn. Health and Stamina restored, +1 skill.")
+    elif action.lower() == "drinking competition":
+        player_stats['health'] -= 5
+        player_stats['stamina'] -= 5
+        player_stats['skill'] += 2
+        print("You participated in the drinking competition. -5 health, -5 stamina, +2 skill.")
+    elif action.lower() == "go back to the field":
+        print("Going back to the field.")
+        #field()
+    #elif action.lower() == "go to the mountain":
+        #mountain()
