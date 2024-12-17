9e95d132d3a934e26c08a3772ebffd90c25bd2df
Emily Ames <emily.ames@maine.edu>
2024-12-17 17:32:26 -0500

    Update inventory.py
    hopefully fixed inventory to write and read to a file to be utilized in main.py file

--- inventory.py
+++ inventory.py
@@ -4,16 +4,29 @@ inventory = {    #dictionary to store player inventory
     "items": []
 }
 
+#load inventory from a file
+def load_inventory():
+    try:
+        with open('inventory.txt', 'r') as file:
+            inventory["items"] = file.read().splitlines()
+    except FileNotFoundError:
+        print("No saved inventory found, starting with an empty inventory.")
 
-def add_item(item):  #function to add item to inventory
+#save inventory to a file
+def save_inventory():
+    with open('inventory.txt', 'w') as file:
+        for item in inventory["items"]:
+            file.write(item + '\n')
+
+
+#function to add item to inventory
+def add_item(item): 
     inventory["items"].append(item)
 
 def show_inventory():
     if inventory["items"]:
-        inventory_list = ""
-        for item in inventory["items"]:
-            inventory_list += item +", "
-        return inventory_list.strip(", ")
+        inventory_list = ", ".join(inventory["items"])
+        return inventory_list
     else:
         return "No items in inventory"
 
